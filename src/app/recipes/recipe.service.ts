c58761e801f24c66de912529b23a0ebacf59ce51
Pranabesh Nath <pranabeshn@gmail.com>
2018-05-24 12:09:03 +0530

    adding angular

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,7 +8,6 @@ export class RecipeService{
 
   constructor(private slsService: ShoppingListService){}
 
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[]=[
     new Recipe("Testy Recipe","Tasty French Fries","http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg",
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,9 @@
 import {Ingredient} from '../shared/ingredient.model';
+import {Subject} from 'rxjs';
 
 export class ShoppingListService{
+
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
       new Ingredient('Apples',5),
@@ -13,5 +16,11 @@ export class ShoppingListService{
 
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients:Ingredient[]){
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
